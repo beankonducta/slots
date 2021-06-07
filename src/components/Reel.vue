3b7d58ed3da9837236d915d88bac5f13d7b34df5
patrick andrews <patrickxandrews@gmail.com>
2021-06-06 18:21:06 -0600

    add prev and next values to spinner view

--- src/components/Reel.vue
+++ src/components/Reel.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="reel">
+    <div class="small-value">
+      {{ valAt(-1) }}
+    </div>
     <div class="value">
       {{ values[valueSlot] }}
+    </div>
+    <div class="small-value">
+      {{ valAt(1) }}
     </div>
   </div>
 </template>
@@ -13,8 +19,9 @@ export default {
   data() {
     return {
       values: [
-        1, 1, 1, 1, 1, 1, 1.25, 1.25, 1.25, 1.25, 1.5, 1.5, 1.5, 1.75, 1.75,
-        1.75, 2, 2, 2, 2.5, 2.5, 3, 3, 3.5, 3.5, 5, 10, 15,
+        // 1, 1, 1, 1, 1, 1, 1.25, 1.25, 1.25, 1.25, 1.5, 1.5, 1.5, 1.75, 1.75,
+        // 1.75, 2, 2, 2, 2.5, 2.5, 3, 3, 3.5, 3.5, 5, 10, 15,
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
       ],
       valueSlot: 0,
       speed: 10,
@@ -29,6 +36,17 @@ export default {
         if (spins > Math.floor(Math.random() * 100)) this.spin(spins, initial);
       }, (initial - spins) / this.speed);
     },
+    valAt(relativeSlot) {
+      if (this.valueSlot + relativeSlot < 0)
+        return this.values[
+          this.values.length - 1 + this.valueSlot + relativeSlot
+        ];
+      if (this.valueSlot + relativeSlot > this.values.length - 1)
+        return this.values[
+          this.valueSlot + relativeSlot - this.values.length - 1
+        ];
+      return this.values[this.valueSlot + relativeSlot];
+    },
   },
 };
 </script>
@@ -36,5 +54,12 @@ export default {
 <style scoped>
 .reel {
   width: 100%;
+  text-align: center;
+}
+.value {
+  font-size: 2em;
+}
+.value-small {
+  font-size: 1em;
 }
 </style>
--- src/components/Slots.vue
+++ src/components/Slots.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="slots">
-    <Reel ref="reelOne" />
-    <Reel ref="reelTwo" />
-    <Reel ref="reelThree" />
+    <div class="reels">
+      <Reel ref="reelOne" />
+      <Reel ref="reelTwo" />
+      <Reel ref="reelThree" />
+    </div>
+    <div class="spacer"></div>
     <button @click="roll()">Roll</button>
   </div>
 </template>
@@ -24,12 +27,21 @@ export default {
       this.$refs.reelOne.spin(500, 500);
       this.$refs.reelTwo.spin(500, 500);
       this.$refs.reelThree.spin(500, 500);
-    }
+    },
   },
 };
 </script>
 <style scoped>
 .slots {
   width: 100%;
+}
+.reels {
+  width: 25%;
+  display: flex;
+  flex-direction: row;
+  margin: 0 auto;
+}
+.spacer {
+  height: 200px;
 }
 </style>
