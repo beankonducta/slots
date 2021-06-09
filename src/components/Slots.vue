3fc902eaacdef21597334f065a30db620f23871b
patrick <patrickxandrews@gmail.com>
2021-06-09 12:54:36 -0600

    add winnings payout for diagonal and top / bottom lines

--- src/components/Slots.vue
+++ src/components/Slots.vue
@@ -6,9 +6,21 @@
       <Reel ref="reelThree" :speed="12" @spun="spun(2, $event)" />
     </div>
     <div class="spacer"></div>
+    bal: {{ balance }} pay: {{ payout }}
+    <br />
     <button @click="roll()" :disabled="active">Roll</button>
-    bal: {{ balance }}
-    pay: {{ payout }}
+    <br />
+    <br />
+    lines: {{ multiplier }}
+    <br />
+    <button @click="multChange(-1)" :disabled="active">-</button>
+    <button @click="multChange(1)" :disabled="active">+</button>
+    <br />
+    <br />
+    bet: {{ bet }}
+    <br />
+    <button @click="betChange(-10)" :disabled="active">-</button>
+    <button @click="betChange(10)" :disabled="active">+</button>
   </div>
 </template>
 
@@ -44,6 +56,14 @@ export default {
       this.$refs.reelTwo.spin(300, 300);
       this.$refs.reelThree.spin(300, 300);
     },
+    multChange(val) {
+      if (val === 1 && this.multiplier < 3) this.multiplier += val;
+      else if (val === -1 && this.multiplier > 1) this.multiplier += val;
+    },
+    betChange(val) {
+      if (val === 10 && this.bet < 100) this.bet += val;
+      else if (val === -10 && this.bet > 10) this.bet += val;
+    },
     removeBet() {
       this.balance -= this.bet * this.multiplier;
     },
@@ -54,24 +74,31 @@ export default {
       this.active = false;
       this.payoutWinnings();
     },
+    // TODO: Refactor
     payoutWinnings() {
-      this.reels.forEach(val => val.spun = false);
+      this.reels.forEach((val) => (val.spun = false));
+      let payout = 0;
+      for (let i = 0; i < this.reels.length - 1; i++)
+        if (
+          this.reels[0].values[i] === this.reels[1].values[i] &&
+          this.reels[1].values[i] === this.reels[2].values[i]
+        )
+          if (i === 0 || this.multiplier > 1)
+            payout += this.bet * this.multiplier * this.reels[0].values[i];
       if (
-        this.reels[0].values[1] === this.reels[1].values[1] &&
-        this.reels[1].values[1] === this.reels[2].values[1]
-      ) {
-        console.log("paying out");
-        console.log(this.reels[0].values[1]);
-        console.log(this.reels[1].values[1]);
-        console.log(this.reels[2].values[1]);
-        this.payout = this.bet * this.multiplier * this.reels[0].values[1];
-        this.balance += this.payout;
-      } else {
-        console.log("NO MATCH!")
-        console.log(this.reels[0].values[1]);
-        console.log(this.reels[1].values[1]);
-        console.log(this.reels[2].values[1]);
-      }
+        this.reels[0].values[0] === this.reels[1].values[1] &&
+        this.reels[1].values[1] === this.reels[2].values[2] &&
+        this.multiplier >= 3
+      )
+        payout += this.bet * this.multiplier * this.reels[0].values[0];
+      if (
+        this.reels[0].values[2] === this.reels[1].values[1] &&
+        this.reels[1].values[1] === this.reels[2].values[0] &&
+        this.multiplier >= 3
+      )
+        payout += this.bet * this.multiplier * this.reels[0].values[2];
+      this.payout = payout;
+      this.balance += this.payout;
     },
   },
   computed: {
